c5b39c3993512d295b5e6bfe91374404026961b8
m5ba <mbalonkin@gmail.com>
2019-03-23 23:14:34 -0500

    first test impl

--- dictionary-md/DictionaryMd.Tests/UnitTest1.cs
+++ dictionary-md/DictionaryMd.Tests/UnitTest1.cs
@@ -1,13 +1,31 @@
 using System;
 using Xunit;
+using Basicalgorithm.Dictionary;
+using System.Collections.Generic;
 
 namespace DictionaryMd.Tests
 {
     public class UnitTest1
     {
+        private IDictionary<int, string> _dict;
+
+        public UnitTest1()
+        {
+            _dict = new DictionaryMd<int, string>(100);
+            _dict.Add(1,"one");
+            _dict.Add(2, "two");
+            _dict.Add(3, "three");
+            _dict.Add(4, "four");
+
+        }
         [Fact]
         public void Test1()
         {
+            Assert.NotNull(_dict);
+            Assert.Equal("one", _dict[1]);
+            Assert.Equal("two", _dict[2]);
+            Assert.Equal("three", _dict[3]);
+            Assert.Equal("four", _dict[4]);
 
         }
     }
